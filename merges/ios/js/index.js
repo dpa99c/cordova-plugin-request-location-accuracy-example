fd62c1db803be96b6f669e6aec53a3f27120ee50
Dave Alden <dpa99c@gmail.com>
2016-07-19 10:34:11 +0100

    Refine iOS implementation

--- merges/ios/js/index.js
+++ merges/ios/js/index.js
@@ -3,46 +3,76 @@ function onDeviceReady() {
     // Bind events
     $(document).on("resume", onResume);
 
-    $('#app-settings').on("click", function(){
+    $('#app-settings').on("click", function () {
         cordova.plugins.diagnostic.switchToSettings();
     });
 
 
-    function onRequestSuccess(){
-        var msg = "Successfully requested location";
-        console.log(msg);
-        alert(msg);
-        checkState();
+    $('#request-location').on("click", function () {
+        $('#request-location').attr('disabled', 'disabled');
+        cordova.plugins.locationAccuracy.request(function () {
+            handleSuccess("Successfully requested use of location");
+        }, onError);
+    });
+
+    $('#request-authorization').on("click", function () {
+
+        cordova.plugins.diagnostic.isLocationEnabled(function (enabled) {
+            if(enabled){
+                cordova.plugins.diagnostic.isLocationAuthorized(function (authorized) {
+                    if(!authorized){
+                        cordova.plugins.diagnostic.requestLocationAuthorization(function (status) {
+                            handleSuccess("Requested location authorization: authorization was " + status);
+                            checkState();
+                        }, onError, cordova.plugins.diagnostic.locationAuthorizationMode.ALWAYS);
+                    }else{
+                        onError("App is already authorized to use location");
+                    }
+                }, onError);
+            }else{
+                onError("Cannot request location authorization while Location Services is OFF");
+            }
+        }, onError);
+    });
+
+
+    function checkState() {
+        console.log("Checking location state...");
+        cordova.plugins.diagnostic.isLocationAvailable(function (available) {
+            $('#location-available').text(available ? "AVAILABLE" : "UNAVAILABLE");
+        }, onError);
+        cordova.plugins.diagnostic.isLocationEnabled(function (enabled) {
+            $('#location-enabled').text(enabled ? "ENABLED" : "DISABLED");
+            if(enabled){
+                cordova.plugins.diagnostic.isLocationAuthorized(function (authorized) {
+                    $('#location-authorized').text(authorized ? "AUTHORIZED" : "UNAUTHORIZED");
+                }, onError);
+                $('#request-location').attr('disabled', 'disabled');
+            }else{
+                $('#location-authorized').text("UNKNOWN");
+                $('#request-location').removeAttr('disabled');
+            }
+        }, onError);
+
     }
 
-    function onRequestFailure(error){
-        var msg = "Request failed: "+error;
+    function onError(error) {
+        var msg = "An error occurred: " + error;
         console.error(msg);
         alert(msg);
         checkState();
     }
 
-    $('#request-accuracy').on("click", function(){
-        cordova.plugins.locationAccuracy.request(onRequestSuccess, onRequestFailure);
-    });
+    function handleSuccess(msg){
+        console.log(msg);
+        alert(msg);
+        checkState();
+    }
+
+    function onResume() {
+        checkState();
+    }
 
     checkState();
 }
-
-
-function checkState(){
-    console.log("Checking location state...");
-    cordova.plugins.diagnostic.isLocationAvailable(function(available){
-        $('#location').text(available ? "AVAILABLE": "UNAVAILABLE");
-    }, onError);
-}
-
-function onError(error){
-    console.error("An error occurred: "+error);
-}
-
-function onResume(){
-    checkState();
-}
-
 $(document).on("deviceready", onDeviceReady);
