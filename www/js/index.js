1353abdb41af6c5f1fba26811ed7dd967b3209a6
Dave Alden <dpa99c@gmail.com>
2018-06-11 20:53:54 +0100

    crossplatform tweaks

--- www/js/index.js
+++ www/js/index.js
@@ -18,65 +18,76 @@ function openSettings(){
     }
 }
 
-
-function handleLocationAuthorizationStatus(cb, status) {
+function handleLocationAuthorizationStatus(status) {
     switch (status) {
         case cordova.plugins.diagnostic.permissionStatus.GRANTED:
-            cb(true);
+            if(platform === "ios"){
+                onError("Location services is already switched ON");
+            }else{
+                _makeRequest();
+            }
             break;
         case cordova.plugins.diagnostic.permissionStatus.NOT_REQUESTED:
-            requestLocationAuthorization(cb);
+            requestLocationAuthorization();
             break;
         case cordova.plugins.diagnostic.permissionStatus.DENIED:
-            cb(false);
+            if(platform === "android"){
+                onError("User denied permission to use location");
+            }else{
+                _makeRequest();
+            }
             break;
         case cordova.plugins.diagnostic.permissionStatus.DENIED_ALWAYS:
             // Android only
-            cb(false);
+            onError("User denied permission to use location");
             break;
         case cordova.plugins.diagnostic.permissionStatus.GRANTED_WHEN_IN_USE:
             // iOS only
-            cb(true);
+            onError("Location services is already switched ON");
             break;
     }
 }
 
-function requestLocationAuthorization(cb) {
-    cordova.plugins.diagnostic.requestLocationAuthorization(handleLocationAuthorizationStatus.bind(this, cb), onError);
+function requestLocationAuthorization() {
+    cordova.plugins.diagnostic.requestLocationAuthorization(handleLocationAuthorizationStatus, onError);
 }
 
-function ensureLocationAuthorization(cb) {
-    cordova.plugins.diagnostic.getLocationAuthorizationStatus(handleLocationAuthorizationStatus.bind(this, cb), onError);
+function ensureLocationAuthorization() {
+    cordova.plugins.diagnostic.getLocationAuthorizationStatus(handleLocationAuthorizationStatus, onError);
+}
+
+function _makeRequest(){
+    cordova.plugins.locationAccuracy.canRequest(function(canRequest){
+        if (canRequest) {
+            cordova.plugins.locationAccuracy.request(function () {
+                    handleSuccess("Location accuracy request successful");
+                }, function (error) {
+                    onError("Error requesting location accuracy: " + JSON.stringify(error));
+                    if (error) {
+                        // Android only
+                        onError("error code=" + error.code + "; error message=" + error.message);
+                        if (platform === "android" && error.code !== cordova.plugins.locationAccuracy.ERROR_USER_DISAGREED) {
+                            if (window.confirm("Failed to automatically set Location Mode to 'High Accuracy'. Would you like to switch to the Location Settings page and do this manually?")) {
+                                cordova.plugins.diagnostic.switchToLocationSettings();
+                            }
+                        }
+                    }
+                }, cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY // iOS will ignore this
+            );
+        } else {
+            // On iOS, this will occur if Location Services is currently on OR a request is currently in progress.
+            // On Android, this will occur if the app doesn't have authorization to use location.
+            onError("Cannot request location accuracy");
+        }
+    });
 }
 
 function requestLocationAccuracy(){
     ensureLocationAuthorization(function(isAuthorized){
         if(isAuthorized){
-            cordova.plugins.locationAccuracy.canRequest(function(canRequest){
-                if (canRequest) {
-                    cordova.plugins.locationAccuracy.request(function () {
-                            handleSuccess("Location accuracy request successful");
-                        }, function (error) {
-                            onError("Error requesting location accuracy: " + JSON.stringify(error));
-                            if (error) {
-                                // Android only
-                                onError("error code=" + error.code + "; error message=" + error.message);
-                                if (platform === "android" && error.code !== cordova.plugins.locationAccuracy.ERROR_USER_DISAGREED) {
-                                    if (window.confirm("Failed to automatically set Location Mode to 'High Accuracy'. Would you like to switch to the Location Settings page and do this manually?")) {
-                                        cordova.plugins.diagnostic.switchToLocationSettings();
-                                    }
-                                }
-                            }
-                        }, cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY // iOS will ignore this
-                    );
-                } else {
-                    // On iOS, this will occur if Location Services is currently on OR a request is currently in progress.
-                    // On Android, this will occur if the app doesn't have authorization to use location.
-                    onError("Cannot request location accuracy");
-                }
-            });
+
         }else{
-            onError("User denied permission to use location");
+
         }
     });
 }
